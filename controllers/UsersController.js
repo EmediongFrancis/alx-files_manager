7143e70822aa73bcf4737156578e9b41bdc1070b
Emediong Francis <emediongfrancis@gmail.com>
2022-06-15 18:57:45 +0100

    Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 import Queue from 'bull';
 import dbClient from '../utils/db';
+import userUtils from '../utils/user';
 
 const userQueue = new Queue('userQueue');
 
@@ -56,6 +58,31 @@ class UsersController {
     });
 
     return response.status(201).send(user);
+  }
+
+  /**
+   *
+   * Should retrieve the user base on the token used
+   *
+   * Retrieve the user based on the token:
+   * If not found, return an error Unauthorized with a
+   * status code 401
+   * Otherwise, return the user object (email and id only)
+   */
+  static async getMe(request, response) {
+    const { userId } = await userUtils.getUserIdAndKey(request);
+
+    const user = await userUtils.getUser({
+      _id: ObjectId(userId),
+    });
+
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
+    const processedUser = { id: user._id, ...user };
+    delete processedUser._id;
+    delete processedUser.password;
+
+    return response.status(200).send(processedUser);
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 function controllerRouting(app) {
   const router = express.Router();
@@ -23,6 +24,23 @@ function controllerRouting(app) {
   // should create a new user in DB
   router.post('/users', (req, res) => {
     UsersController.postNew(req, res);
+  });
+
+  // should retrieve the user base on the token used
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
+
+  // Auth Controller
+
+  // should sign-in the user by generating a new authentication token
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  // should sign-out the user based on the token
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
   });
 }
 
